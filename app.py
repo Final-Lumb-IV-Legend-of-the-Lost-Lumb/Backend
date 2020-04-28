a495f5151c53be7903e8e0148cd56c54b1cd5531
Tim Griffith <grifmang@gmail.com>
2020-04-28 12:50:24 -0400

    fix attempt

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
-    session.clear()
+    # session.clear()
 
     if request.method == 'POST':
 
@@ -63,7 +63,7 @@ def logout():
 def register():
     error = None
 
-    session.clear()
+    # session.clear()
 
     if request.method == 'POST':
 
@@ -85,15 +85,15 @@ def register():
             error = 'Username in use. Please choose another.'
             return redirect(url_for('register'))
 
-        user = User('username', 'password')
-        session.add(user)
-        session.commit()
+        # user = User('username', 'password')
+        # session.add(user)
+        # session.commit()
 
         #original code
-        # db.execute("INSERT INTO users (username, password) VALUES(:username, :password)", {"username": request.form.get("username"), "password": bc.generate_password_hash(request.form.get('password'))}).decode('utf-8')
-        # newRows = db.execute("SELECT * FROM users WHERE username = :username", {"username": request.form.get('username')}).fetchall()
-        # session['user_id'] = newRows[0]['username']
-        # db.commit()
+        db.execute("INSERT INTO users (username, password) VALUES(:username, :password)", {"username": request.form.get("username"), "password": bc.generate_password_hash(request.form.get('password'))}).decode('utf-8')
+        newRows = db.execute("SELECT * FROM users WHERE username = :username", {"username": request.form.get('username')}).fetchall()
+        session['user_id'] = newRows[0]['username']
+        db.commit()
 
         
         flash('Registered')
