abba0fe3abfda5c1677847fb4a15e480b7840057
Tim Griffith <grifmang@gmail.com>
2020-04-28 17:24:44 -0400

    take three

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from models import Users, RevokedTokenModel
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
-    return Users.RevokedTokenModel.is_jti_blacklisted(jti)
+    return RevokedTokenModel.is_jti_blacklisted(jti)
 
 @app.route('/')
 def home():
