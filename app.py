f3b074ae4ca23835059901ccdb95e55614c18682
Tim Griffith <grifmang@gmail.com>
2020-04-28 13:53:58 -0400

    jeff please again

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def login():
 
         # rows = db.execute("SELECT * FROM users WHERE username = :user", {"user": request.form.get('username')}).fetchall()
 
-        registered_user = Users.query.filter_by(username=username)
+        registered_user = Users.query.filter_by(username=username).first()
         print(registered_user.username)
 
         # if not registered_user or not bc.check_password_hash(request.form.get('password'), registered_user['password']):
