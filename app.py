0061ae589e683191fb1867f86150c5ab87f95559
Tim Griffith <grifmang@gmail.com>
2020-04-28 16:25:23 -0400

    models fix

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bc = Bcrypt(app)
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
