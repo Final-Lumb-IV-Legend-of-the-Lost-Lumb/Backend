f61580d02b6e725763d771dd9bd8e9405d12e8b4
Tim Griffith <grifmang@gmail.com>
2020-04-29 22:46:22 -0400

    styling changes for chat

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def login():
         return render_template('login.html')
 
 @app.route('/add-item', methods=['POST'])
+@jwt_required
 def addItem():
     data = json.loads(request.data) # load JSON data from request
     pusher.trigger('item', 'item-added', data) # trigger 'item-added' event on 'item' channel
