51495baf2f5742a30b6de67deb4977742e6d889c
Tim Griffith <grifmang@gmail.com>
2020-04-29 23:54:41 -0400

    kwargs fix

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def addItem():
     username = get_jwt_identity()
     data = json.loads(request.data) # load JSON data from request
     pusher.trigger('item', 'item-added', data) # trigger 'item-added' event on 'item' channel
-    return jsonify(data, username=username)
+    return jsonify(data, username)
 
 @app.route("/logout", methods=['POST'])
 @jwt_required
