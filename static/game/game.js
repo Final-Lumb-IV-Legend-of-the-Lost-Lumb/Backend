3eda95662a9bc18e110cd745b850f72964d5d96c
ryankayne <coope1rk@gmail.com>
2020-04-30 23:16:28 -0400

    icons loaded

--- static/game/game.js
+++ static/game/game.js
@@ -1,24 +1,23 @@
-import Player from './player.js'
-import MapBuilder from './map.js'
-import { data } from './data.js'
-import Controller from './controller.js'
+import Player from "./player.js";
+import MapBuilder from "./map.js";
+import { data } from "./data.js";
+import Controller from "./controller.js";
+import Collectible from "./collectible.js";
 
 export default class Game {
-    constructor(gameWidth, gameHeight){
-        this.gameWidth = gameWidth;
-        this.gameHeight = gameHeight;
-        this.player = new Player(this);
-        this.map = data.map1
-        
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.player = new Player(this);
+    this.collectible = new Collectible(this);
+    this.map = data.map2;
 
-        new Controller(this.player, this);
-    }
+    new Controller(this.player, this);
+  }
 
-    draw(ctx){
-        MapBuilder(ctx, this.map)
-        this.player.draw(ctx)
-
-    }
-
-
+  draw(ctx) {
+    MapBuilder(ctx, this.map);
+    this.player.draw(ctx);
+    this.collectible.draw(ctx);
+  }
 }
--- static/game/index.js
+++ static/game/index.js
@@ -1,16 +1,15 @@
-import Game from './game.js'
+import Game from "./game.js";
 
-
-let canvas = document.getElementById("gameScreen")
-let ctx = canvas.getContext("2d")
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
 
 const game_width = 800;
 const game_height = 800;
 
-let game = new Game(game_width, game_height)
+let game = new Game(game_width, game_height);
 
-ctx.clearRect(0,0,800,800);
+ctx.clearRect(0, 0, 800, 800);
 
-game.draw(ctx)
+game.draw(ctx);
 
 // game.start()
