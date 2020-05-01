b53b2821363361da88c32a927fa638e44dc7cf69
Tim Griffith <grifmang@gmail.com>
2020-04-30 21:05:45 -0400

    model fix

--- models.py
+++ models.py
@@ -8,14 +8,13 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique = True, nullable = False)
     password = db.Column(db.String(), nullable = False)
-    inventory = db.relationship('PlayerInventory', backref='players', lazy=True)
+    
     money = db.Column(db.Integer(), nullable = False)
     
 
-    def __init__(self, username, password, money, inventory={}):
+    def __init__(self, username, password, money):
         self.username = username
         self.password = password
-        self.inventory = inventory
         self.money = money
 
     def __repr__(self):
@@ -46,10 +45,15 @@ class Items(db.Model):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key = True)
+
     item_name = db.Column(db.String(120), nullable=False)
+    
     buying_value = db.Column(db.Integer(), nullable = False)
+    
     selling_value = db.Column(db.Integer(), nullable = False)
+    
     attribute = db.Column(db.String(120), nullable=False)
+    
     item_type = db.Column(db.String(120), nullable=False)
 
     def __init__(self, item_name, buying_value, selling_value, attribute, item_type ):
@@ -61,16 +65,17 @@ class Items(db.Model):
 
 class PlayerInventory(db.Model):
     __tablename__ = 'player_inventory'
-    item_id = db.Column(db.String(120), db.ForeignKey('items.id'), unique = True,
-        nullable=False)
-    username = db.Column(db.String(120), db.ForeignKey('users.username'), unique = True,
-        nullable=False)
-    item_name = db.Column(db.String(120), db.ForeignKey('items.item_name'), unique = True,
-        nullable=False)
+    item_id = db.Column(db.String(120), db.ForeignKey('items.id'), unique=True, nullable=False)
+    
+    user_id = db.Column(db.String(120), db.ForeignKey('users.id'), unique=True, nullable=False)
+    
+    item_name = db.Column(db.String(120), unique = True, nullable=False)
+    
     quantity = db.Column(db.Integer(), nullable = False)
     
     def __init__(self, quantity):
         self.quantity = quantity
+        self.item_name = item_name
 
 # class RoomInventory(db.Model):
 #     __tablename__ = 'room_inventory'
